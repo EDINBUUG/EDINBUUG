a2dc8d271828743a6da5699a330f929b7d6f6684
Steven Harland <stvnhrlnd@gmail.com>
2017-07-15 22:26:27 +0100

    Add next meetup to home page

---
+++ EDINBUUG.Site/BLL/MeetupLogic.cs
@@ -0,0 +1,35 @@
+﻿using EDINBUUG.Site.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace EDINBUUG.Site.BLL
+{
+    public class MeetupLogic
+    {
+        /// <summary>
+        /// Gets the next upcoming meetup from the Meetup API.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Meetup"/> object or <c>null</c> if there are no upcoming meetups.
+        /// </returns>
+        public static Meetup GetNextMeetup()
+        {
+            Meetup meetup = null;
+
+            using (var client = new HttpClient())
+            {
+                var result = client.GetAsync($"http://api.meetup.com/Edinburgh-Umbraco-Users-Group/events").Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var content = result.Content.ReadAsStringAsync().Result;
+                    var meetups = JsonConvert.DeserializeObject<IEnumerable<Meetup>>(content);
+                    meetup = meetups.FirstOrDefault();
+                }
+            }
+
+            return meetup;
+        }
+    }
+}
---
+++ EDINBUUG.Site/Models/Meetup.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace EDINBUUG.Site.Models
+{
+    /// <summary>
+    /// Represents a Meetup Event.
+    /// </summary>
+    public class Meetup
+    {
+        /// <summary>
+        /// Gets or sets the link to event on meetup.com.
+        /// </summary>
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UTC start time of the event, in milliseconds since the epoch.
+        /// </summary>
+        [JsonProperty("time")]
+        public ulong Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets the local offset from UTC time, in milliseconds.
+        /// </summary>
+        [JsonProperty("utc_offset")]
+        public ulong UtcOffset { get; set; }
+
+        /// <summary>
+        /// Gets the actual date/time of the event, calculated based on the UTC start time and offset.
+        /// </summary>
+        public DateTime DateTime
+        {
+            get
+            {
+                var epoch = new DateTime(1970, 1, 1, 0, 0, 0);
+                return epoch.AddMilliseconds(Time + UtcOffset);
+            }
+        }
+    }
+}
